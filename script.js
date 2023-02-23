77cd8e1ac29359fcc5ff97bbc13eea1dce45c601
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-02-23 17:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+let btn = document.getElementById('btn');
+let output = document.getElementById('respond');
+let num = document.getElementById('num');
+function genRandomNum() {
+    document.getElementById("num").innerHTML =Math.floor( Math.random()*40-20);
+}
+
+btn.addEventListener('click',function(){
+    let input = document.getElementById('guess').value;
+    if (input == num) {
+        output.innerHTML = 'Hot'
+    }else if(input<num){
+        output.innerHTML = 'Cold'
+    }
+    if(input>num){
+        output.innerHTML='Cold'
+    }
+});
 
